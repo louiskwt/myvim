8bac35443d9d56ce927abef62773579e0fd53ba7
Louis Tsang <kawingt2@gmail.com>
2023-07-15 23:17:34 +0800

    cleaned up and updated init.lua

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,5 +1,4 @@
 -- Option config
-
 vim.opt.clipboard = 'unnamedplus' -- use system clipboard
 vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
 vim.opt.mouse = 'a' --allow the mouse to be used in Nvim
